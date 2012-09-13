26e657f47d008c6c3bddf170f7bd1b6ac4732203
Inversion <yura.des@gmail.com>
2012-09-14 01:20:58 +0300

    v2.0.0 changes - (Opera) fixed: иногда скрипт крашится из-за того, что $ еще не определен на момент исполнения скрипта (сообщение от хабраюзера CaptainFlint) - fixed: скрипт не срабатывал, если около имени автора топика есть ссылка G+ - changed: цвет подсветки изменен на менее насыщен (замечание от CaptainFlint) - код сильно изменен, из-за чего увеличена мажорная версия

--- topic_author_highlighter.user.js
+++ topic_author_highlighter.user.js
@@ -1,51 +1,67 @@
 // Topic Author Highlighter
-// v1.0.1 (14.04.12)
 // by Yuriy Babak aka Inversion (http://inversion.habrahabr.ru/), mailto: yura.des@gmail.com
 // based on http://userscripts.org/scripts/show/29991 by TiGR
 
 // ==UserScript==
 // @name         Topic Author Highlighter
-// @version      1.0.1
+// @version      2.0.0
 // @namespace    Habrahabr
 // @description  Подсвечивает комментарии автора топика
 // @include      http://habrahabr.ru/*
 // ==/UserScript==
 
-/*
-	v1.0.1 (14.04.12)
-		- добавлена поддержка Opera
-		
-	v1.0 (18.03.12)
+/* --------------------------------------------------------------------------------
 
-*/
+v2.0.0 (14.09.12)
+- (Opera) fixed: иногда скрипт крашится из-за того, что $ еще не определен на момент исполнения скрипта (сообщение от хабраюзера CaptainFlint)
+- fixed: скрипт не срабатывал, если около имени автора топика есть ссылка G+
+- changed: цвет подсветки изменен на менее насыщен (замечание от CaptainFlint)
+- код сильно изменен, из-за чего увеличена мажорная версия
+
+v1.0.1 (14.04.12)
+- добавлена поддержка Opera
+	
+v1.0 (18.03.12)
+- public release
+
+---------------------------------------------------------------------------------- */
+
 "use strict";
-window.unsafeWindow = ( typeof unsafeWindow == 'undefined' ? window : unsafeWindow )
-!function(win, uwin, doc, $) {
+
+!function(win) {
+	if (window != window.top) return
+	var doc = win.document
 	
-	if (win != win.top) return
-	
-	// on ready
-	$(function(){
-	
-		// comments present on page
-		if ($('#comments')[0]) {
-		
-			$('<style>\
-				.info.topic_author {background-color:#FDE4E0} \
-				.info.topic_author.is_new {background-color:#efd9ef !important} \
-			</style>').appendTo('head')
+	// waiting for jQuery
+	var t_waiting_for_jQuery = setInterval(function() {
+		if (win.jQuery) {
+			clearInterval(t_waiting_for_jQuery)
+			var $ = win.jQuery
+
+			// on ready
+			$(function(){
 			
-			var authorName = $('.infopanel .author a, div.vcard .nickname span').text()
+				// comments present on page
+				if ($('#comments')[0]) {
+				
+					$('<style>\
+						.info.topic_author {background-color:#FFEFEF} \
+						.info.topic_author.is_new {background-color:#efd9ef !important} \
+					</style>').appendTo('head')
+					
+					var authorName = $('.infopanel .author a:first, div.vcard .nickname span').text()
+					
+					$('#comments div.info .username, #comments ul.info .username a').each(function(){
+						var uName = $(this)
+						if (uName.text() == authorName) {
+							uName.parent('.info').addClass('topic_author')
+							uName.parents('ul.info:first').addClass('topic_author')
+						}
+					})
+				}	// comments present on page
 			
-			$('#comments div.info .username, #comments ul.info .username a').each(function(){
-				var uName = $(this)
-				if (uName.text() == authorName) {
-					uName.parent('.info').addClass('topic_author')
-					uName.parents('ul.info:first').addClass('topic_author')
-				}
-			})
-		}	// comments present on page
-	
-	})	// on ready
+			})	// on ready
+		} // if (win.jQuery)
+	}, 50) // setInterval
 	    
-}(window, unsafeWindow, unsafeWindow.document, unsafeWindow.jQuery)
+}(unsafeWindow || window)
